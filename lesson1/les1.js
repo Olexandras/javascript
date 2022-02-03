78376eb959ac1c77d0e89d9e0e5eeb2dcf1b8dbb
Olexandras <olexandrayaremko@gmail.com>
2022-02-03 18:30:04 +0200

    homework1js

---
+++ lesson1/les1.js
@@ -0,0 +1,140 @@
+let a = `hello`;
+console.log(a);
+alert(a);
+document.write(a);
+a = `bye`;
+console.log(a);
+alert(a);
+document.write(a);
+
+let b = `owu`;
+console.log(b);
+alert(b);
+document.write(b);
+b = `liet`;
+console.log(b);
+alert (b);
+document.write(b);
+
+let c = `com`;
+console.log(c);
+alert(c);
+document.write(`<div>${c}</div>`);
+c = `net`
+console.log (c);
+alert(c);
+document.write(`<div>${c}</div>`);
+//
+let d = `ua`;
+console.log(d);
+alert(d);
+document.write(`<div>${d}</div>`);
+d = `en`;
+console.log(d);
+alert(d);
+document.write(`<div>${d}</div>`);
+let e = 1;
+console.log(e);
+alert(e);
+document.write(`<div>${e}</div>`);
+e = 2;
+console.log(e);
+alert(e);
+document.write(`<div>${e}</div>`);
+
+let f = 10;
+console.log(f);
+alert(f);
+document.write(`<div>${f}</div>`);
+f = 20;
+console.log(f);
+alert(f);
+document.write(`<div>${f}</div>`);
+
+let g = -999;
+console.log(g);
+alert(g);
+document.write(`<div>${g}</div>`);
+g = 999;
+console.log(g);
+alert(g);
+document.write(`<div>${g}</div>`);
+//
+let h = 123;
+console.log(h);
+alert(h);
+document.write(`<div>${h}</div>`);
+h = 321;
+console.log(h);
+alert(h);
+document.write(`<div>${h}</div>`);
+
+let i = 3.14;
+console.log(i);
+alert(i);
+document.write(`<div>${i}</div>`);
+i = 5;
+console.log(i);
+alert(i);
+document.write(`<div>${i}</div>`);
+
+let j = 2.7;
+console.log(j);
+alert(j);
+document.write(`<div>${j}</div>`);
+j = 6.9;
+console.log(j);
+alert(j);
+document.write(`<div>${j}</div>`);
+
+let k = 16;
+console.log(k);
+alert(k);
+document.write(`<div>${k}</div>`);
+k = 32;
+console.log(k);
+alert(k);
+document.write(`<div>${k}</div>`);
+
+let l = true;
+console.log(l);
+alert(l);
+document.write(`<div>${l}</div>`);
+l = false;
+console.log(l);
+alert(l);
+document.write(`<div>${l}</div>`);
+let m = false;
+console.log(m);
+alert(m);
+document.write(`<div>${m}</div>`);
+m = true;
+console.log(m);
+alert(m);
+document.write(m);
+
+let firstName = `Lesya`;
+let middleName = `Lvivna`;
+let lastName = `Yaremko`;
+
+ let person =[firstName, middleName, lastName];
+ console.log (person);
+
+let hi;
+hi = prompt(`Вітаю Леся Львівна!`);
+let old = prompt('Тобі 30 років?');
+
+
+console.log(5<6);
+console.log(5===6);
+console.log(5>=6);
+console.log(5>6);
+console.log(10==10);
+console.log(10===10);
+console.log(10<10);
+console.log(10!=10);
+console.log(10<=10);
+console.log(123>`123`);
+console.log(123==`123`);
+
+

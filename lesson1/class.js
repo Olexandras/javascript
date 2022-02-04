d6a4e99b04a4a4cc5496eef86297600cf60401be
Olexandras <olexandrayaremko@gmail.com>
2022-02-04 09:17:46 +0200

    classwork1js

--- lesson1/class.js
+++ lesson1/class.js
@@ -5,14 +5,13 @@ let book = {
     pages: 145,
     genre: `detective`};
 console.log (book);
-// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
 let book1 = {
     title: `A history of Ukraine`,
     pages: 345,
     genre: `history`,
     authors:{
-        author1: `Stepan`,
-        author2: `Petro`,
+        author1: `Stepan Sarno`,
+        author2: `Petro Bilyj`,
 
     }
 
@@ -42,8 +41,7 @@ let heightC = 10;
 let dC = 4;
 let v = (3.14*heightC*dC);
 console.log(v);
-// У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
-// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
+
  let n = Math.pow(3,2);
  let m = Math.pow(4,2);
  let k = ( n + m);
